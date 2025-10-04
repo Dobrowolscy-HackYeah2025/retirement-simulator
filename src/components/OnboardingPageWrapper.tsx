e43a3a2a09f775ba6ab50f33c63cfc8111756851
jszuminski <jakubszuminski0@gmail.com>
2025-10-04 18:18:03 +0200

    gobackbuttonwork

--- src/components/OnboardingPageWrapper.tsx
+++ src/components/OnboardingPageWrapper.tsx
@@ -1,14 +1,21 @@
 import ZusLogo from '@/assets/zus_logo.svg';
 
 import { ChevronLeft } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from './ui/button';
 
 const OnboardingNavigation = () => {
+  const navigate = useNavigate();
+
+  const handleGoToMainPage = () => {
+    navigate('/');
+  };
+
   return (
     <div className="flex justify-between mx-2 mt-2">
       <div className="flex gap-2 justify-start items-center">
-        <Button variant="ghost" size="icon-lg">
+        <Button variant="ghost" size="icon-lg" onClick={handleGoToMainPage}>
           <ChevronLeft />
         </Button>
         <img src={ZusLogo} className="w-16 h-16 mt-1" />
