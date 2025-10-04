35a029b0d57b0cfffa375541b1b945b082b5d7e5
jszuminski <jakubszuminski0@gmail.com>
2025-10-04 23:33:06 +0200

    wave

--- src/routes/landing.tsx
+++ src/routes/landing.tsx
@@ -14,19 +14,19 @@ const cards = [
   {
     title: 'Dokładne prognozy',
     description:
-      'Obliczenia oparte na oficjalnych danych ZUS i parametrach demograficznych GUS',
+      'Obliczenia oparte na oficjalnych danych ZUS i parametrach demograficznych GUS.',
     icon: <CheckCircleIcon className="size-6 text-primary" />,
   },
   {
     title: 'Różne scenariusze',
     description:
-      'Zobacz jak zmieni się Twoja emerytura w zależności od wieku przejścia i stażu pracy',
+      'Zobacz jak zmieni się Twoja emerytura w zależności od wieku przejścia i stażu pracy.',
     icon: <TrendingUpIcon className="size-6 text-primary" />,
   },
   {
     title: 'Bezpieczeństwo danych',
     description:
-      'Wszystkie obliczenia wykonywane są lokalnie w Twojej przeglądarce',
+      'Wszystkie obliczenia wykonywane są lokalnie w Twojej przeglądarce.',
     icon: <ShieldCheckIcon className="size-6 text-primary" />,
   },
 ];
@@ -81,7 +81,7 @@ export function LandingPage() {
                 <h3 className="text-base md:text-lg font-semibold text-foreground mb-2">
                   {card.title}
                 </h3>
-                <p className="text-xs md:text-base text-muted-foreground mix-blend-difference">
+                <p className="text-xs md:text-base text-muted-foreground mix-blend-plus-lighter">
                   {card.description}
                 </p>
               </div>
@@ -89,20 +89,18 @@ export function LandingPage() {
           ))}
         </div>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-          className="fixed bottom-0 left-0 right-0 -z-10"
-        >
-          <path
-            fill="#35a454"
-            fill-opacity="1"
-            d="M0,128L40,138.7C80,149,160,171,240,181.3C320,192,400,192,480,176C560,160,640,128,720,112C800,96,880,96,960,117.3C1040,139,1120,181,1200,208C1280,235,1360,245,1400,250.7L1440,256L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-          ></path>
-        </svg>
+        <div className="fixed -bottom-16 left-0 right-0 -z-10 overflow-hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#35a454"
+              fill-opacity="1"
+              d="M0,64L80,96C160,128,320,192,480,208C640,224,800,192,960,192C1120,192,1280,224,1360,240L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            ></path>
+          </svg>
+        </div>
 
         <div className="flex w-full justify-center fixed bottom-0 left-0">
-          <p className="max-w-7xl mx-auto text-sm text-white/80 left-0 w-full py-4 text-left px-4">
+          <p className="max-w-7xl mx-auto text-xs text-white/80 left-0 w-full py-2 text-left px-4">
             Kalkulator ma charakter informacyjny i nie stanowi oficjalnej
             prognozy ZUS.
           </p>
