133442446cbf14dc4768a8c1a882806d6e6747d4
jszuminski <jakubszuminski0@gmail.com>
2025-10-04 19:39:53 +0200

    add clean navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,22 @@
-import { Button } from '@/components/ui/button';
-import { useRetirementReport } from '@/lib/report';
 import { AboutPage } from '@/routes/about';
 import Dashboard from '@/routes/dashboard';
 import { HomePage } from '@/routes/index';
 import { OnboardingPage } from '@/routes/onboarding';
 import { Onboarding2SalaryPage } from '@/routes/onboarding-2-salary';
 
-import { useCallback } from 'react';
-
-import { HashRouter, Link, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom';
 
 import { OnboardingProgressBar } from './components/OnboardingProgressBar';
+import { PageNavigationBar } from './components/PageNavigationBar';
 
 const AppContent = () => {
   const location = useLocation();
-  const generateRetirementReport = useRetirementReport();
-
-  const handleGenerateReport = useCallback(() => {
-    void generateRetirementReport();
-  }, [generateRetirementReport]);
 
   return (
     <div className="h-full w-full">
       {location.pathname.includes('onboarding') && <OnboardingProgressBar />}
 
-      {!location.pathname.includes('onboarding') && (
-        <nav className="p-2 flex gap-2 text-lg border-b">
-          <Link to="/">
-            <Button variant="ghost">Home</Button>
-          </Link>
-          <Link to="/about">
-            <Button variant="ghost">About</Button>
-          </Link>
-          <Link to="/onboarding">
-            <Button variant="ghost">Onboarding</Button>
-          </Link>
-          <Button onClick={handleGenerateReport}>Generate raport</Button>
-          <Link to="/dashboard">
-            <Button variant="ghost">Dashboard</Button>
-          </Link>
-        </nav>
-      )}
+      {!location.pathname.includes('onboarding') && <PageNavigationBar />}
 
       <Routes>
         <Route path="/" element={<HomePage />} />
---
+++ src/components/PageNavigationBar.tsx
@@ -0,0 +1,35 @@
+import ZusLogo from '@/assets/zus_logo.svg';
+import { Button } from '@/components/ui/button';
+import { useRetirementReport } from '@/lib/report';
+
+import { useCallback } from 'react';
+
+import { Link } from 'react-router-dom';
+
+export const PageNavigationBar = () => {
+  const generateRetirementReport = useRetirementReport();
+
+  const handleGenerateReport = useCallback(() => {
+    void generateRetirementReport();
+  }, [generateRetirementReport]);
+
+  return (
+    <nav className="border-b border-primary/5 bg-white shadow-sm shadow-green-200">
+      <div className="flex items-center justify-between px-6 py-3">
+        <Link to="/" className="flex items-center gap-2">
+          <img src={ZusLogo} className="h-12 w-12" alt="ZUS Logo" />
+        </Link>
+
+        {/* Navigation links on the right */}
+        <div className="flex items-center gap-2">
+          <Button
+            onClick={handleGenerateReport}
+            className="bg-primary hover:bg-primary/90 text-white"
+          >
+            Generuj raport PDF
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
