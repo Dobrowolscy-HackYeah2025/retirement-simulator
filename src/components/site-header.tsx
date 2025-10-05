2b6934a0dc1206a4161c2ae0cd8458f8d891bda9
jszuminski <jakubszuminski0@gmail.com>
2025-10-05 08:08:20 +0200

    siteheaderzuslogo

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,3 +1,5 @@
+import ZusLogo from '@/assets/zus_logo.svg';
+
 import { AiBanner } from './ai/AiBanner';
 import { GenerateReportCtaButton } from './GenerateReportCtaButton';
 
@@ -5,13 +7,14 @@ export function SiteHeader() {
   return (
     <header className="flex shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear ">
       <div className="flex flex-col w-full">
-        <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:py-6 lg:px-6">
+        <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:py-4 lg:px-6">
           {/* <SidebarTrigger className="-ml-1" /> */}
           {/* <Separator
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         /> */}
-          <div className="lg:text-2xl text-base font-medium">
+          <img src={ZusLogo} className="h-12 w-12 mt-0.5" alt="ZUS Logo" />
+          <div className="lg:text-xl text-base font-medium ml-2">
             Twoja prognoza emerytalna
           </div>
           <div className="ml-auto flex items-center gap-2">
